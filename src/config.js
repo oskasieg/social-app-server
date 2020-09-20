caf36c834ba69d4ccfd7f344ae6fbd1eefc68c4e
Krzysztof <47390597+oskasieg@users.noreply.github.com>
2020-09-20 14:20:18 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const baseConfig = {
+  port: 8000,
+  dbUrl: 'mongodb://localhost:27017/social-app',
+  secrets: {
+    jwt: 'learneverything',
+    jwtExp: '100d',
+  },
+};
+
+module.exports = baseConfig;
